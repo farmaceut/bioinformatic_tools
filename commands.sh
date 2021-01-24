1d41539e8a1e039402f4466c3519eeb7434dd4a1
Maciej Spiegel <maciej.spiegel@student.umed.wroc.pl>
2021-01-24 12:05:51 +0100

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,3 +1,7 @@
 # Find .sdf molecules, optimize them with Open-Babel and convert to VINA-friendly .pdbqt format
 
-find ~+ -name "*.sdf" |  while read line; do cd "$(dirname "${line}")"; obminimize "$(basename "${line}")" > "$(basename "${line}" | cut -f 1 -d '.')"_opt.sdf; obabel "$(basename "${line}" | cut -f 1 -d '.')"_opt.sdf -O "$(basename "${line}" | cut -f 1 -d '.')"_opt.pdbqt; done
+find ~+ -name "*.sdf" |  while read line; do
+  cd "$(dirname "${line}")";
+  obminimize "$(basename "${line}")" > "$(basename "${line}" | cut -f 1 -d '.')"_opt.sdf;
+  obabel $(basename ${line} | cut -f 1 -d '.')_opt.sdf -O $(basename ${line} | cut -f 1 -d '.')_opt.pdbqt;
+done
